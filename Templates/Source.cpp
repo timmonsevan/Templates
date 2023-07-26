3a0b5c32e91fe1de7c850bf0055065ac0dc17033
timmonsevan <timmonsevan@gmail.com>
2023-07-26 13:38:08 -0400

    changed name of int half template to intHalf because could not overload the template

--- Templates/Source.cpp
+++ Templates/Source.cpp
@@ -13,14 +13,20 @@ T half(T number)
 }
 
 template <typename T>
-int half(T number)
+int intHalf(T number)
 {
 	return round(static_cast<double>(number / 2));
 }
 
 int main()
 {
+	double a = 6.66;
+	float b = 14.42;
+	int c = 9;
 
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << intHalf(c) << endl;
 
 
 	return 0;
