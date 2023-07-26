b9e22a7f17fc54bbbaf4b09cf837747489c96354
timmonsevan <timmonsevan@gmail.com>
2023-07-26 14:00:12 -0400

    could not get the template version of int half to work so removed the template syntax and it works as described

--- Templates/Source.cpp
+++ Templates/Source.cpp
@@ -12,22 +12,21 @@ T half(T number)
 	return number / 2;
 }
 
-template <typename T>
-T half(int number)
+int half(int number)
 {
-	return round(static_cast<double>(number / 2));
+	double x = static_cast<double>(number);
+	return round(x / 2);
 }
 
 int main()
 {
 	double a = 7.0;
 	float b = 13.0;
-	int c = 9;
+	int c = 3;
 
 	cout << half(a) << endl;
 	cout << half(b) << endl;
 	cout << half(c) << endl;
-
 
 	return 0;
 }
