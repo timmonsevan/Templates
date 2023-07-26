34f12a03df877d2579b2d8ce2f389745c5ec01e9
timmonsevan <timmonsevan@gmail.com>
2023-07-26 13:33:44 -0400

    added template for double and floats, began template for int, added cmath header file

--- Templates/Source.cpp
+++ Templates/Source.cpp
@@ -3,9 +3,20 @@
 // 7/26/23
 
 #include<iostream>
+#include<cmath>
 using namespace std;
 
+template <typename T>
+T half(T number)
+{
+	return number / 2;
+}
 
+template <typename T>
+int half(T number)
+{
+	
+}
 
 int main()
 {
